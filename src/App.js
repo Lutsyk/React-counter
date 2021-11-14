a9ee21c9f67ca73d42f6c7ce241f9f88dfbc1323
Max Lutsenko <makc199710@me.com>
2021-11-14 18:16:18 +0100

    add useRef
    useRef count render

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React, {useState, useEffect, useRef} from 'react';
 
 
 function App() {
+  const [value, setValue] = useState('text')
+  const renderCount = useRef(1)
 
+  useEffect(()=>{
+    renderCount.current++
+  })
 
   return (
     <div className="container">
 
-      <h1></h1>
-      <div>
-        <button className="btn">Users</button>
-        <button className="btn">Todo</button>
-        <button className="btn">Posts</button>
-      </div>
-
+      <h1>Render count: {renderCount.current}</h1>
+      <input type='text' onChange={e => setValue(e.target.value)} value={value}/>
     </div>
   );
 }
