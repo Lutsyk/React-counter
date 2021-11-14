12dc6d2d9cabf8280c52a7b2fec71d311b615f60
Max Lutsenko <makc199710@me.com>
2021-11-14 18:26:24 +0100

    add focus
    add focus on input with useRef

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import React, {useState, useEffect, useRef} from 'react';
 function App() {
   const [value, setValue] = useState('text')
   const renderCount = useRef(1)
+  const inputRef = useRef(null)
 
   useEffect(()=>{
     renderCount.current++
+    console.log(inputRef.current.value)
   })
+
+  const focus = () => inputRef.current.focus()
 
   return (
     <div className="container">
 
       <h1>Render count: {renderCount.current}</h1>
-      <input type='text' onChange={e => setValue(e.target.value)} value={value}/>
+      <input ref={inputRef} type='text' onChange={e => setValue(e.target.value)} value={value}/>
+      <button className="btn" onClick={focus}>Focus</button>  
     </div>
   );
 }
