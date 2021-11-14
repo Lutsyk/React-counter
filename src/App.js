c8322745b62e8c51c0997363dd97c95d30d88b92
Max Lutsenko <makc199710@me.com>
2021-11-15 00:03:15 +0100

    finished create own Hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
+
+function useLogger(value) {
+  useEffect(()=>{
+    console.log(value)
+  },[value])
+}
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = event => {
+    setValue(event.target.value)
+  }
+
+  const clear = () => setValue('')
+
+  return {
+    bind:{value, onChange},
+    value,
+    clear
+  }
+}
 
 function App() {
+  
+  const input = useInput('')
+
+  useLogger(input.value)
+
   return (
 
       <div className="container">
-
+        <input type="text" {...input.bind}/>
+        <button className='btn' onClick={()=> input.clear()}>Clear</button>
+        <h1>{input.value}</h1>
       </div>
   );
 }
