479486885d100b4e7b52d93059bc1d262da12b94
Max Lutsenko <makc199710@me.com>
2021-11-14 17:43:32 +0100

    useEffect
    add useEffect and api practice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 
 function App() {
 
+  const [type, setType] = useState('users')
+  const [data, setData] = useState([])
 
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then(response => response.json())
+      .then(json => setData(json))
+  }, [type])
 
   return (
     <div className="container">
 
+      <h1>Resorse: {type}</h1>
+      <div>
+        <button className="btn" onClick={()=>setType('users')}>Users</button>
+        <button className="btn" onClick={()=>setType('todos')}>Todo</button>
+        <button className="btn" onClick={()=>setType('posts')}>Posts</button>
+      </div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
 
-      
     </div>
   );
 }
