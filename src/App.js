a31d1d8a0e17cd5395378334b43a76d14950fdb7
Max Lutsenko <makc199710@me.com>
2021-11-14 18:31:36 +0100

    useMemo
    useMemo practice

--- src/App.js
+++ src/App.js
@@ -2,29 +2,10 @@ import React, {useState, useEffect, useRef} from 'react';
 
 
 function App() {
-  const [value, setValue] = useState('text')
-  const renderCount = useRef(1)
-  const inputRef = useRef(null)
-  const prevValue = useRef('')
-
-  useEffect(()=>{
-    renderCount.current++
-    console.log(inputRef.current.value)
-  })
-
-  useEffect(()=> {
-    prevValue.current = value
-  }, [value])
-
-  const focus = () => inputRef.current.focus()
 
   return (
     <div className="container">
-
-      <h1>Render count: {renderCount.current}</h1>
-      <h2>Prev Render: {prevValue.current}</h2>
-      <input ref={inputRef} type='text' onChange={e => setValue(e.target.value)} value={value}/>
-      <button className="btn" onClick={focus}>Focus</button>  
+      
     </div>
   );
 }
