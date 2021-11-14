a80e8bd403e2944142de00484dfb8027181a0f70
Max Lutsenko <makc199710@me.com>
2021-11-14 18:29:37 +0100

    add prevValue
    add prevValue

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ function App() {
   const [value, setValue] = useState('text')
   const renderCount = useRef(1)
   const inputRef = useRef(null)
+  const prevValue = useRef('')
 
   useEffect(()=>{
     renderCount.current++
     console.log(inputRef.current.value)
   })
+
+  useEffect(()=> {
+    prevValue.current = value
+  }, [value])
 
   const focus = () => inputRef.current.focus()
 
@@ -17,6 +22,7 @@ function App() {
     <div className="container">
 
       <h1>Render count: {renderCount.current}</h1>
+      <h2>Prev Render: {prevValue.current}</h2>
       <input ref={inputRef} type='text' onChange={e => setValue(e.target.value)} value={value}/>
       <button className="btn" onClick={focus}>Focus</button>  
     </div>
