6eeb91c94b477a0800b8d3214bf660312da4ca4e
Max Lutsenko <makc199710@me.com>
2021-11-14 17:19:26 +0100

    sprint 3
    add folder hooks with hooks practice
    - state practice
    and start practice with useEffect

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
 
 
-function App() {
-  const [counter, setCounter] = React.useState(0)
-
-  function increment(){
-    setCounter(counter+1)
-  }
-
-  function decrtment() {
-    setCounter(counter-1)
-  }
 
 
   return (
     <div className="container">
 
-      <h1>Counter = {counter}</h1>
 
-      <div className="btnDiv">
-        <button onClick={increment} className="btn increment">+</button>
-        <button onClick={decrtment} className="btn decrtment">–</button>
-      </div>
       
     </div>
   );
